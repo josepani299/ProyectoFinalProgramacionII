fc562cb5f9b722165b99138a8bc650e8027e27a7
Jose <paniaguajose299@gmail.com>
2025-06-14 16:36:54 -0300

    Agregue estructura mvc al proyecto

--- ProyectoFinalProgramacionII/src/main/java/com/mycompany/proyectofinalprogramacionii/Main.java
+++ ProyectoFinalProgramacionII/src/main/java/com/mycompany/proyectofinalprogramacionii/Main.java
@@ -8,7 +8,7 @@ package com.mycompany.proyectofinalprogramacionii;
  *
  * @author Usuario
  */
-public class ProyectoFinalProgramacionII {
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
