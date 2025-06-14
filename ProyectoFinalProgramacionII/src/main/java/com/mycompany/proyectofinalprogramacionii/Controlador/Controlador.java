474c73defaeebc0ab49e2b9488e439408419c406
Jose <paniaguajose299@gmail.com>
2025-06-14 18:11:50 -0300

    Puso codigo en la vista "agregue para poder ingresar string en vista" Modifique el controlador "Se puede crear un equipo"

--- ProyectoFinalProgramacionII/src/main/java/com/mycompany/proyectofinalprogramacionii/Controlador/Controlador.java
+++ ProyectoFinalProgramacionII/src/main/java/com/mycompany/proyectofinalprogramacionii/Controlador/Controlador.java
@@ -3,9 +3,56 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.proyectofinalprogramacionii.Controlador;
+import com.mycompany.proyectofinalprogramacionii.Modelos.Equipo;
+import com.mycompany.proyectofinalprogramacionii.Modelos.Jugador;
+import com.mycompany.proyectofinalprogramacionii.Modelos.Torneo;
+import com.mycompany.proyectofinalprogramacionii.Vista.Vista;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
 
 
 public class Controlador {
+    private Vista vista;
+    private List<Torneo> torneos = new ArrayList<>();
+    private List<Equipo> equipos = new ArrayList<>();
+    private List<Jugador> jugadores = new ArrayList<>();
+    // falta crear las listas de Rol, mapas, personajes
+    
+
+    public Controlador(Vista vista) {
+        this.vista = vista;
+    }
+    
+    // Creamos el metodo para crear equipo
+    
+    public void crearEquipo(){
+        vista.mostrarTexto("Ingrese el nombre del equipo");
+        String nombreEquipo = vista.pedirString();
+        vista.mostrarTexto("Ingrese el pais de origen del equipo");
+        String paisEquipo = vista.pedirString();
+        vista.mostrarTexto("Ingrese el nombre del coach del equipo");
+        String nombreCoach = vista.pedirString();
+        vista.mostrarTexto("Ingrese la fecha de creacion aa/mm/dd");
+        String fecha =vista.pedirString();
+        
+        // Establecemos el tipo de formato para presetnar en el tipo local date y lo convertimos a el tipo Local Date
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("yy/MM/dd");
+        LocalDate fechaCreacion = LocalDate.parse(fecha,formato);
+        
+        //Creamos el objeto equipo y lo agregamos a la lista de equipos
+        
+        Equipo e = new Equipo(nombreEquipo,paisEquipo,fechaCreacion, nombreCoach);
+        equipos.add(e);
+        vista.mostrarTexto("Se agrego el equipo");
+        
+        
+    }
+    
+    
+    
+    
     
     
 }
--- ProyectoFinalProgramacionII/src/main/java/com/mycompany/proyectofinalprogramacionii/Modelos/Equipo.java
+++ ProyectoFinalProgramacionII/src/main/java/com/mycompany/proyectofinalprogramacionii/Modelos/Equipo.java
@@ -1,13 +1,91 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.proyectofinalprogramacionii.Modelos;
 
-/**
- *
- * @author Usuario
- */
+package com.mycompany.proyectofinalprogramacionii.Modelos;
+import java.time.LocalDate;
+import java.util.*;
+
+
 public class Equipo {
+    private String nombre;
+    private String pais;
+    private LocalDate fechaCreacion;
+    private List<Jugador> jugadoresEquipo = new ArrayList<>();
+    private List<Torneo> torneosJugados = new ArrayList<>();
+    private String coach;
+    private int cantidadDeTorenoGanadador;
+
+    public Equipo() {
+    }
+
+    public Equipo(String nombre, String pais, LocalDate fechaCreacion, String coach) {
+        this.nombre = nombre;
+        this.pais = pais;
+        this.fechaCreacion = fechaCreacion;
+        this.coach = coach;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public LocalDate getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    
+
+    public List<Torneo> getTorneosJugados() {
+        return torneosJugados;
+    }
+
+    public String getCoach() {
+        return coach;
+    }
+
+    public int getCantidadDeTorenoGanadador() {
+        return cantidadDeTorenoGanadador;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public void setFechaCreacion(LocalDate fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public void setCoach(String coach) {
+        this.coach = coach;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipo{" + "nombre=" + nombre + ", pais=" + pais + ", fechaCreacion=" + fechaCreacion + ", coach=" + coach + ", cantidadDeTorenoGanadador=" + cantidadDeTorenoGanadador + '}';
+    }
+    //Metodo para mostrar los valores de la lista de jugadores
+    public void mostrarJugadoresEquipo(){
+        for (Jugador j: jugadoresEquipo){
+            System.out.println(j.toString());
+        }
+    }
+    
+    // Metodo para mostrar la lista de torneos jugados
+    public void mostrarTorneosJugados(){
+        for (Torneo t: torneosJugados){
+            System.out.println(t.toString());
+        }
+    }
+    
+    
+    
+    
+    
     
 }
